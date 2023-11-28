b44f0dba982e3396af41cdce4bcd7aaa00b01eef
gurelselin <selin.gurel@std.medipol.edu.tr>
2023-11-28 20:33:15 +0300

    Bugun Derste yapılan calismam eklenmistir

---
+++ JavaProjeOrnek/src/tr/edu/medipol/yazilim/Main.java
@@ -0,0 +1,20 @@
+package tr.edu.medipol.yazilim;
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String hataliMetin = " M ed i p     o         l";
+		System.out.println("Hatali metin: " +hataliMetin);
+		
+		
+		String duzgunMetin = StringUtils.deleteWhitespace(hataliMetin);
+		System.out.println("Duzgun metin: " +duzgunMetin);
+		
+		
+
+
+	}
+
+}
